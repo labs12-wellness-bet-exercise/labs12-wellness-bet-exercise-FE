f9ac37aff8070fa5d6434f51e1e88064583a5e93
Patrick <patrick.schwindt14@gmail.com>
2019-05-03 11:06:45 -0600

    Chnaged a couple names

--- wellness-bet-exercise/src/components/App/index.js
+++ wellness-bet-exercise/src/components/App/index.js
@@ -13,13 +13,11 @@ class App extends Component {
   
 
   componentDidMount() {
-    this.getCharacters('https://swapi.co/api/people');
+    this.getGroups('https://swapi.co/api/people');
   }
 
-  getCharacters = URL => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
+  getGroups = URL => {
+  
     fetch(URL)
       .then(res => {
         return res.json();
