16bec116cb7e49052b9bc086eb098173dc18f1c4
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-03 11:48:58 -0500

    created dummy profile page component

---
+++ wellness-bet-exercise/src/components/Profile/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// individual profile page 
+
+class Profile extends React.Component {
+
+    render() {
+        <div>
+            <h1>NAME GOES HERE</h1>
+            <div>IMAGE GOES HERE</div>
+            <p>EMAIL ADDRESS GOES HERE</p>
+            <button>Update Profile</button>
+        </div>
+    }
+} 
+
+export default Profile;
