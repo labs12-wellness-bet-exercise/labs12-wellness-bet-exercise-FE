41f4f88e13cad507759a2523cc400b7462ff8d44
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-06 15:53:07 -0500

    got rid of signup from the app imports

--- wellness-bet-exercise/src/components/App/index.js
+++ wellness-bet-exercise/src/components/App/index.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
-import SignUpPage from '../SignUp';
 import SignInPage from '../SignIn';
 import PasswordForgetPage from '../PasswordForget';
 import AdminPage from '../AdminGroupDash';
