41c59cf60eca4c183622e1eb6f141dcd57395e45
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-09 16:38:17 -0500

    getting logic done

--- wellness-bet-exercise/src/components/AddOrJoinGroup/GroupJoin/GroupJoin.js
+++ wellness-bet-exercise/src/components/AddOrJoinGroup/GroupJoin/GroupJoin.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 class GroupJoin extends React.Component {
+
+
+  checkJoinCode = (code) => {
+      if (code === joinCode) {
+
+      } else {
+         
+      }
+  } 
+
   render(){
     return(
       <h1>Enter code to join a group</h1>
+      <form onSubmit={this.checkJoinCode}>
+        <input placeholder="join code"/>
+        <button type="submit">submit join code</button>
+      </form>
     )
   }
 }
