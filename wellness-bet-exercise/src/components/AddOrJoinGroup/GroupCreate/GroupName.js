1b14e34c8355fd9b29c9648f432466b6fe678a52
Joe Bugajski <yogurtmystery@gmail.com>
2019-05-10 15:29:10 -0500

    Group form roughed in up to Group Photo

---
+++ wellness-bet-exercise/src/components/AddOrJoinGroup/GroupCreate/GroupName.js
@@ -0,0 +1,51 @@
+import React from "react";
+import StartEndDate from './StartEndDate'
+
+class GroupName extends React.Component {
+  state = {
+    group_name: '',
+    groupNameAdded: false
+  }
+
+  nameAddedToggle = e => {
+    e.preventDefault()
+    this.setState({
+      ...this.state,
+      groupNameAdded: !this.stategroupNameAdded
+    })
+  }
+  
+  handleChange = e => {
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    })
+  }
+  render(){
+    return(
+      <div>
+        {this.state.groupNameAdded === false ?
+          <form>
+            <input
+              type="text" 
+              name="group_name"
+              value={this.state.group_name}
+              placeholder="Enter Group Name" 
+              onChange={this.handleChange} 
+            />
+            {this.state.group_name.length > 0 ?
+              <button onClick={this.nameAddedToggle}>
+                Next
+              </button>: <p className='fadedNext'>Next</p> }
+          </form>: 
+              <StartEndDate  
+                group_name={this.state.group_name}
+                nameAddedToggle={this.nameAddedToggle} 
+              />
+        }
+      </div>
+    )
+  }
+}
+
+export default GroupName;
