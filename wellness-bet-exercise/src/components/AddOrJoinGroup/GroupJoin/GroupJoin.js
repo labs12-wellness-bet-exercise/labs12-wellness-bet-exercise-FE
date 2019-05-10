55687c38e4e16c8868b952fc452b0af18f90dd8c
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-10 13:22:30 -0500

    got join form written

--- wellness-bet-exercise/src/components/AddOrJoinGroup/GroupJoin/GroupJoin.js
+++ wellness-bet-exercise/src/components/AddOrJoinGroup/GroupJoin/GroupJoin.js
@@ -5,22 +5,63 @@ class GroupJoin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      join_code: ""
+      join_code: "",
+      error: "",
+      group_id: ""
     };
   }
+
+  checkJoinCode = () => {
+    axios
+      .get(
+        `https://wellness-bet.herokuapp.com/api/joincodes/${
+          this.state.join_code
+        }`
+      )
+      .then(res => {
+        this.setState({
+          group_id: res.body.group_id
+        });
+      })
+      .catch(error => {
+        console.log("there was an error", error);
+        this.setState({
+          error
+        });
+      });
+  };
 
   handleChanges = e => {
     this.setState({
       [e.target.name]: e.target.value
     });
+    this.checkJoinCode();
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("https://wellness-bet.herokuapp.com/api/participants/", {
+        group_id: this.state.group_id,
+        user_id: this.state.user_id
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => console.log(error));
   };
 
   render() {
     return (
       <div>
         <h1>Enter code to join a group</h1>
-        <form onSubmit={this.checkJoinCode}>
-          <input placeholder="join code" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="join code"
+            name="join_code"
+            onChange={this.handleChanges}
+            value={this.state.join_code}
+          />
           <button type="submit">submit join code</button>
         </form>
       </div>
