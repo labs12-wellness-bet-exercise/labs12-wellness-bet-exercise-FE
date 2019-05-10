f88a30e0730449be56017e432588681a0223eb16
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-05-10 09:31:19 -0500

    working on group form- committing to pull changes from master

--- wellness-bet-exercise/src/components/AddOrJoinGroup/GroupJoin/GroupJoin.js
+++ wellness-bet-exercise/src/components/AddOrJoinGroup/GroupJoin/GroupJoin.js
@@ -1,24 +1,30 @@
 import React from "react";
+import axios from "axios";
 
 class GroupJoin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      join_code: ""
+    };
+  }
 
+  handleChanges = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-  checkJoinCode = (code) => {
-      if (code === joinCode) {
-
-      } else {
-         
-      }
-  } 
-
-  render(){
-    return(
-      <h1>Enter code to join a group</h1>
-      <form onSubmit={this.checkJoinCode}>
-        <input placeholder="join code"/>
-        <button type="submit">submit join code</button>
-      </form>
-    )
+  render() {
+    return (
+      <div>
+        <h1>Enter code to join a group</h1>
+        <form onSubmit={this.checkJoinCode}>
+          <input placeholder="join code" />
+          <button type="submit">submit join code</button>
+        </form>
+      </div>
+    );
   }
 }
 
