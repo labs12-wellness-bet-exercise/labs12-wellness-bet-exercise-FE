071cc2bee28b44bc0972906e046435408a504225
lydiathornton <lydia.d.thornton@gmail.com>
2019-05-10 17:22:28 -0700

    pulling in group data by user

--- wellness-bet-exercise/src/components/App/index.js
+++ wellness-bet-exercise/src/components/App/index.js
@@ -1,50 +1,45 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import * as ROUTES from '../../constants/routes';
-import SignInPage from '../SignIn';
-import Home from '../Home'
-import fire from '../../config/fire.js';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+import SignInPage from "../SignIn";
+import Home from "../Home";
+import fire from "../../config/fire.js";
 
-class App extends React.Component { 
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user: {}
-    }
+    };
   }
 
   componentDidMount = () => {
     this.authListener();
-  }
+  };
 
   authListener = () => {
     fire.auth().onAuthStateChanged(user => {
       if (user) {
-        this.setState({ user }); 
-      } 
-      else {
+        this.setState({ user });
+      } else {
         this.setState({
           user: null
-        })
+        });
       }
-    })
-  }
+    });
+  };
 
-  render(){
-    return(
+  render() {
+    return (
       <Router>
         <div>
-          <div>
-            {this.state.user ? <Home /> : <SignInPage /> }
-          </div>
+          <div>{this.state.user ? <Home /> : <SignInPage />}</div>
           <Route path={ROUTES.HOME} component={Home} />
           <Route path={ROUTES.SIGN_IN} component={SignInPage} />
         </div>
       </Router>
-    )
+    );
   }
 }
-  
-  
 
 export default App;
