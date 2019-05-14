e2abed397a6003989fc9db96b37cdfd44426d2a3
Patrick <patrick.schwindt14@gmail.com>
2019-05-14 11:56:29 -0600

    Payment Proof Set-Up, need to handle CORS error

--- wellness-bet-exercise/src/components/Payment/paymentProof.js
+++ wellness-bet-exercise/src/components/Payment/paymentProof.js
@@ -1,22 +1,52 @@
-import React from 'react';
-import FileDrop from 'react-file-drop';
+import Dropzone from "react-dropzone";
+import axios from "axios";
+import React from "react";
 
-class ReactFileDropDemo extends React.Component {
-  handleDrop = (files, event) => {
-    console.log(files, event);
-  }
-
+class Payment extends React.Component {
   render() {
-    const styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
     return (
-      <div id="react-file-drop-demo" style={{styles}}>
-        <FileDrop onDrop={this.handleDrop}>
-          Drop some files here!
-        </FileDrop>
+      <div>
+        <Dropzone onDrop={this.handleDrop}>
+          {({ getRootProps, getInputProps }) => (
+            <section>
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <p>Drag 'n' drop some files here, or click to select files</p>
+              </div>
+            </section>
+          )}
+        </Dropzone>
       </div>
     );
   }
+  handleDrop = files => {
+    // Push all the axios request promise into a single array
+    const uploaders = files.map(file => {
+      // Initial FormData
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("tags", `codeinfuse, medium, gist`);
+      formData.append("upload_preset", "jd4lvk5f"); // Replace the preset name with your own
+      formData.append("api_key", "629151892737318"); // Replace API key with your own Cloudinary key
+      formData.append("timestamp", (Date.now() / 1000) | 0);
+
+      // Make an AJAX upload request using Axios (replace Cloudinary URL below with your own)
+      return axios
+        .post("https://api.cloudinary.com/v1_1/wellness-bet", formData, {
+          headers: { "X-Requested-With": "XMLHttpRequest" }
+        })
+        .then(response => {
+          const data = response.data;
+          const fileURL = data.secure_url; // You should store this URL for future references in your app
+          console.log(data);
+        });
+    });
+
+    // Once all the files are uploaded
+    axios.all(uploaders).then(() => {
+      // ... perform after upload is successful operation
+    });
+  };
 }
 
-export default ReactFileDropDemo;
-//
+export default Payment;
